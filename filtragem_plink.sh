81a46311b91f43b3f875795599b646063c04beb2
João Victor <93877854+juvicat@users.noreply.github.com>
2024-11-10 14:48:57 -0300

    Update filtragem_plink.sh

--- filtragem_plink.sh
+++ filtragem_plink.sh
@@ -8,9 +8,9 @@ bcftools annotate --set-id +"%CHROM:%POS" "$prefix".snp.filtered.nocall.vcf > "$
 
 # Filtragem
 # Dados faltantes e desequilíbrio de ligação: 
-/mnt/c/Users/Lagevol/Desktop/programas/plink_linux_x86_64_20231211/plink --vcf-filter --vcf "$prefix".snp.filtered.nodot.vcf --allow-extra-chr --recode  --make-bed --geno 0 --const-fid --out "$prefix"
-/mnt/c/Users/Lagevol/Desktop/programas/plink_linux_x86_64_20231211/plink --indep 50 5 2 --file "$prefix" --allow-extra-chr --out "$prefix"
-/mnt/c/Users/Lagevol/Desktop/programas/plink_linux_x86_64_20231211/plink --extract "$prefix".prune.in --out "$prefix"_pruned --file "$prefix" --make-bed --allow-extra-chr --recode
+plink --vcf-filter --vcf "$prefix".snp.filtered.nodot.vcf --allow-extra-chr --recode  --make-bed --geno 0 --const-fid --out "$prefix"
+plink --indep 50 5 2 --file "$prefix" --allow-extra-chr --out "$prefix"
+plink --extract "$prefix".prune.in --out "$prefix"_pruned --file "$prefix" --make-bed --allow-extra-chr --recode
 
 # Geração de valores individuais para PCAs de 20 eixos:
 plink --pca 20 --file "$prefix"_pruned --allow-extra-chr --out "$prefix"
